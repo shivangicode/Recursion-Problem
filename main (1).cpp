5d5c16c194c5d5a150e3c1fe9fb4016084fa8552
shivangicode <121848725+shivangicode@users.noreply.github.com>
2023-01-03 15:43:16 +0530

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,30 @@
+#include<iostream> 
+using namespace std;
+
+void reachHome(int src, int dest) {
+    
+    cout << "source " << src  << " destination " << dest << endl;
+  
+    if(src == dest) {
+        cout << " pahuch gya " << endl;
+        return ;
+    }
+
+    src++;
+  
+    reachHome(src, dest);
+
+}
+
+int main() {
+
+    int dest = 10;
+    int src = 1;
+
+    cout << endl;
+
+    reachHome(src, dest);
+
+
+    return 0;
+}
